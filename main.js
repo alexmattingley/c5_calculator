f37fdb06778ca7ebb82812095caa86de4b3d00ea
Alex Mattingley <alexmattingley@gmail.com>
2015-07-22 11:07:15 -0700

    0.1[complete]

--- main.js
+++ main.js
@@ -8,5 +8,5 @@ function add_numbers(){
   var number_0 = $("#number_0").val();
   var number_1 = $('#number_1').val();
   var result = parseInt(number_0) + parseInt(number_1);
-  $("#result").val(result);     
+  $("#result_display").text(result);     
 }
