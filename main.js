37453a9f90131f1b9d426b010ecf069ad03f15ce
Alex Mattingley <alexmattingley@gmail.com>
2015-11-11 11:05:57 -0800

    small stuff

--- main.js
+++ main.js
@@ -235,20 +235,6 @@ function create_calc_array() {
   }
   console.log(calculate_array);
   final_number = 0;
-  switch(calculate_array[1]) {//beginning of switch
-      case '+'://triggers on plus symbol
-        add_numbers();//calls add numbers function
-        break;
-      case '-'://triggers on minus symbol
-        sub_numbers();//calls subtract function
-        break;
-      case '*'://triggers on multiply
-        mul_numbers();//calls multiply function
-        break;
-      case '/'://triggers on divide
-        div_numbers();//calls divide function
-        break;
-    }
 }
 
 /********************
