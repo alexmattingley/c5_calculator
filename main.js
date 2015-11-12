0fe62c83d100de01b5dca4d8d1e054232133032c
Alex Mattingley <alexmattingley@gmail.com>
2015-11-12 08:15:21 -0800

    the operation switch is not running

--- main.js
+++ main.js
@@ -158,10 +158,9 @@ function operator_switch(relevant_array) {
 
 //creation of add_numbers function
 function add_numbers(relevant_array){
-  if(!isNaN(relevant_array[0])){
-    final_number = relevant_array[0] + relevant_array[2];
-    operation_helper(relevant_array);
-  }
+  final_number = relevant_array[0] + relevant_array[2];
+  operation_helper(relevant_array);
+  console.log('op switch running');
 }
 // creation of sub_numbers function
 function sub_numbers(relevant_array){
@@ -172,6 +171,7 @@ function sub_numbers(relevant_array){
 function mul_numbers(relevant_array){
   final_number = relevant_array[0] * relevant_array[2];
   operation_helper(relevant_array);
+  console.log('op switch running');
 };
 //creation of div_numbers function
 function div_numbers(relevant_array){
@@ -255,7 +255,7 @@ function create_calc_array() {
     for(var x = 0; x < calculate_array.length-1; x++){
      operator_switch(calculate_array);
      console.log(calculate_array);
-     console.log('final_number' , final_number);
+     console.log('im running!' , final_number);
     }
   }else {
     operator_switch(calculate_array);
